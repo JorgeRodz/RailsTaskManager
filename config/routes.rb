fc03a0f1f38b735edeeb503290e1b811329c71d6
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-02-08 11:37:56 -0600

    Establish Task index as our root app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tasks
   resources :categories
+
+  # para indicar que se abra el index de Task cuando se accede al directorio raiz en el navegador osea -> localhost:3000
+  root 'tasks#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
